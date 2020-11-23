87776b8e4c76e4d93bc2cfc24bbc472eb212de71
milanpetrovic <milanmushin@gmail.com>
2020-11-23 03:42:38 +0100

    Reading and Writing Files Asynchronously

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 
-// const inputContent = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(inputContent);
+// Blocking, synchronous way
+/* const inputContent = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(inputContent);
+const outputContent = `This is what we know about avocado: ${inputContent}.\nCreated on ${Date()}`;
+fs.writeFileSync('./txt/output.txt', outputContent); */
 
-// Asynchronous function instead of synchronous - non-blocking model
-fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-    console.log(data);
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written');
+            });
+        });
+    });
 });
 console.log('Reading file...');
-
-// const outputContent = `This is what we know about avocado: ${inputContent}.\nCreated on ${Date()}`;
-// fs.writeFileSync('./txt/output.txt', outputContent);
