a6f2f2e021b879db64922b65d76925e54bc3233e
milanpetrovic <milanmushin@gmail.com>
2020-11-23 03:14:08 +0100

    Asynchronous Nature of Node.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
-const inputContent = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(inputContent);
+// const inputContent = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(inputContent);
 
-const outputContent = `This is what we know about avocado: ${inputContent}.\nCreated on ${Date()}`;
-fs.writeFileSync('./txt/output.txt', outputContent);
+// Asynchronous function instead of synchronous - non-blocking model
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
+console.log('Reading file...');
+
+// const outputContent = `This is what we know about avocado: ${inputContent}.\nCreated on ${Date()}`;
+// fs.writeFileSync('./txt/output.txt', outputContent);
