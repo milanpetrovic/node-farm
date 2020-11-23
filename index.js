01057732fc04e1d2a13f37fa289bbed1fb941749
milanpetrovic <milanmushin@gmail.com>
2020-11-24 00:20:44 +0100

    HTML Templating Filling the Templates

--- index.js
+++ index.js
@@ -18,19 +18,51 @@ const url = require('url');
 // console.log('Reading file...');
 
 
-///////////////////////////////////// SERVER //////////////////////////////////////////
+///////////////////////////////////// SERVER /////////////////////////////////////////
+const replaceTemplate = (tmpl, product) => {
+    let output = tmpl.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    if (!product.organic) { 
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    } else {
+        output = output.replace(/{%NOT_ORGANIC%}/g, '');
+    }
+
+    return output;
+};
+
+const tmplOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tmplProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tmplCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((request, response) => {
     const pathName = request.url;
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        response.end('This is the OVERVIEW');
+        response.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataObject.map(el => replaceTemplate(tmplCard, el)).join('');
+        const output = tmplOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+        response.end(output);
+
+    // Product page
     } else if (pathName === '/product') {
-        response.end('This is the PRODUCT');
+        response.end('Product page');
+
+    // API
     } else if (pathName === '/api') {
         response.writeHead(200, {'Content-type': 'application/json'});
         response.end(data);
+
+    // 404
     } else {
         response.writeHead(404, {
             'Content-type': 'text/html'
