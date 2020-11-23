0b281e08aaa9da90291159ad1b804d2b32a9a48d
milanpetrovic <milanmushin@gmail.com>
2020-11-23 04:15:52 +0100

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
+const http = require('http');
 
-// Blocking, synchronous way
-/* const inputContent = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(inputContent);
-const outputContent = `This is what we know about avocado: ${inputContent}.\nCreated on ${Date()}`;
-fs.writeFileSync('./txt/output.txt', outputContent); */
+//////////////////////////// FILES READING / WRITING //////////////////////////////////
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written');
+//             });
+//         });
+//     });
+// });
+// console.log('Reading file...');
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written');
-            });
-        });
-    });
+
+
+///////////////////////////////////// SERVER //////////////////////////////////////////
+const server = http.createServer((request, response) => {
+    response.end('Hello from the server');
 });
-console.log('Reading file...');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
